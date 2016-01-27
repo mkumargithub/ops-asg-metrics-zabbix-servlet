62e953da04ebf76b3f44f614b65a352181d0be58
mkumaromnifone <mkumar@omnifone.com>
2016-01-27 11:59:22 +0000

    inital commit

---
+++ src/main/java/com/omnifone/commons/metrics/zabbix/RegexDisableMetricsFilter.java
@@ -0,0 +1,74 @@
+package com.omnifone.commons.metrics.zabbix;
+
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricFilter;
+import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author mkumar
+ */
+
+/**
+ * Matches against names of metrics, filtering unwanted ones out and avoid publishing them.
+ * This class provides regular expression based matching to
+ * the actual reporter class.
+ */
+
+public class RegexDisableMetricsFilter implements MetricFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(RegexDisableMetricsFilter.class);
+    private Pattern disablePattern = null;
+
+    /**
+     * Convenience constructor. Expects a regular expression that indicates which
+     * metrics to disable.
+     */
+
+    public RegexDisableMetricsFilter(String disableRegex) {
+        logger.debug("Predicate: Disabled: [{}]", disableRegex);
+       // Objects.requireNonNull(disableRegex, "MetricsFilter Disable Regex can't be null");
+        if (disableRegex != null && !disableRegex.trim().isEmpty()) {
+            disablePattern = Pattern.compile(disableRegex);
+        }
+    }
+
+    /**
+     * Matches returns true unless the metric name does not match the regular
+     * expression passed to the constructor when creating the predicate matcher.
+     * @return True if the regular expression matches the metric name
+     */
+    @Override
+    public boolean matches(String name, Metric metric) {
+        if (name == null || metric == null) {
+            return false;
+        }
+
+        logger.debug("Predicate.matches : Name[{}] Metric[{}]",
+                name, metric.getClass());
+
+        boolean retVal = true;
+        if (disablePattern != null) {
+
+            //Creating JSON object
+            JSONObject metricsJsonObj = new JSONObject();
+            metricsJsonObj.put("Name", name);
+            metricsJsonObj.put("Metrics", metric.getClass());
+
+            // If it matches, don't send. Otherwise, send.
+           // retVal = !disablePattern.matcher(name).matches();
+
+            retVal = !disablePattern.matcher(metricsJsonObj.toJSONString()).matches();
+            System.out.println("JSON Object: " +metricsJsonObj);
+        }
+
+        logger.debug("Predicate.matches: Name[{}] Metric[{}]: [{}]",
+                name, metric.getClass(), retVal);
+
+        return retVal;
+    }
+}
---
+++ src/main/java/com/omnifone/commons/metrics/zabbix/ZabbixMetricsReporter.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.omnifone.commons.metrics.zabbix;
+
+import com.codahale.metrics.MetricFilter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.SharedMetricRegistries;
+import io.github.hengyunabc.metrics.ZabbixReporter;
+import io.github.hengyunabc.zabbix.sender.ZabbixSender;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.SystemConfiguration;
+import org.apache.commons.configuration.web.ServletContextConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author mkumar
+ */
+@WebListener
+public class ZabbixMetricsReporter implements ServletContextListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ZabbixMetricsReporter.class);
+    private static final String METRIC_REGISTRY_NAME_PROPERTY = "com.omnifone.metrics.registry.name";
+    private static final String METRIC_REGISTRY_NAME_DEFAULT = "OmnifoneMetrics";
+
+    private static final String REPORT_SOURCE_USERNAME = "zabbix.metrics.source.username";
+    private static final String REPORT_PERIOD = "zabbix.metrics.report.period.seconds";
+    private static final int REPORT_PERIOD_SECS_DEFAULT = 60;
+    private static final String HOSTNAME = "zabbix.metrics.hostname";
+    private static final String PORT = "zabbix.metrics.port";
+    private static final int PORT_DEFAULT = 10051;
+
+    /**
+     * Particular metrics are disabled by default but you can override this by specifying a comma
+     * separated list of names
+     */
+    private static final String REPORT_DISABLED = "zabbix.metrics.disabled";
+
+    private ZabbixReporter zabbixReporter;
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        LOG.info("Starting ZabbixMetricsReporter");
+        try {
+            CompositeConfiguration config = new CompositeConfiguration();
+            config.addConfiguration(new SystemConfiguration());
+            config.addConfiguration(new ServletContextConfiguration(event.getServletContext()));
+
+            boolean reportSourceAsUserName = config.getBoolean(REPORT_SOURCE_USERNAME, false);
+            if (reportSourceAsUserName) {
+                LOG.info("Reporting source as username instead of default (hostname)");
+            }
+
+            // Set of reporting periods, by default only report at 5 minute level
+            int reportPeriodSeconds = config.getInt(REPORT_PERIOD, REPORT_PERIOD_SECS_DEFAULT);
+
+            String zabbixHostname = config.getString(HOSTNAME);
+            int zabbixPort = config.getInt(PORT, PORT_DEFAULT);
+
+            // default predicate is everything
+            MetricFilter filter = MetricFilter.ALL;
+
+            // but specific metrics can be disabled by specifying a regexp
+            String metricsFilterDisabledRegex = config.getString(REPORT_DISABLED);
+            if (metricsFilterDisabledRegex != null) {
+                LOG.info("Excluding the reporting of matches to '{}' to zabbix. will report all others", metricsFilterDisabledRegex);
+                filter = new RegexDisableMetricsFilter(metricsFilterDisabledRegex);
+            } else {
+                LOG.info("Reporting all metrics to zabbix");
+            }
+
+
+            if (zabbixHostname != null) {
+                zabbixReporter = new ZabbixReporter.Builder(getRegistry(config))
+                        .hostName(reportSourceAsUserName ? getUsername() : getSource())
+                        .filter(filter)
+                        //                .prefix("")
+                        //                .replacePercentSign("")
+                        .build(new ZabbixSender(zabbixHostname, zabbixPort));
+
+                zabbixReporter.start(reportPeriodSeconds, TimeUnit.SECONDS);
+            } else {
+                LOG.info("ZabbixMetricsReporter not created as Zabbix Hostname was null");
+            }
+        } catch (Throwable t) {
+            LOG.error("Failed to initialize zabbix metrics reporter.", t);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (zabbixReporter != null) {
+            zabbixReporter.stop();
+        }
+    }
+
+    private static MetricRegistry getRegistry(Configuration config) {
+        String registryName = null;
+        try {
+            registryName = config.getString(METRIC_REGISTRY_NAME_PROPERTY);
+
+        } catch (Exception e) {
+            LOG.debug("Unable to retrieve metric registry name from config");
+        }
+
+        return SharedMetricRegistries.getOrCreate(
+                registryName == null
+                        ? METRIC_REGISTRY_NAME_DEFAULT
+                        : registryName);
+    }
+
+    /**
+     * Default source is the canonical host name. However, allow override.
+     *
+     * @return
+     * @throws UnknownHostException
+     */
+    public String getSource() throws UnknownHostException {
+        return InetAddress.getLocalHost().getCanonicalHostName();
+    }
+
+    private String getUsername() {
+        return System.getProperty("user.name");
+    }
+}
